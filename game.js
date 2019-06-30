4354bb8ba3e05cad55f3626bbbe682505260565f
killdummy <48837311+killdummy@users.noreply.github.com>
2019-07-01 00:15:11 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 window.onload = init;
 
-var ctxMap, map, widthMap = 900, heightMap = 600;
+var ctxMap, map, widthMap = 1200, heightMap = 600;
 var imageOpponent = new Image();
 imageOpponent.src = "https://forums.nexusmods.com/uploads/profile/photo-thumb-36912170.png?r_=1505345044";
 var tiles1 = new Image();
@@ -10,9 +10,10 @@ fireball.src = "https://vignette.wikia.nocookie.net/planetcentauri/images/f/fa/S
 var isPlaying;
 
 var bullet = [];
+var enemies = [];
 var bullets = 0, timer = 0;
 
-var i;
+var i, j;
 
 var requestAnimFrame = window.requestAnimationFrame ||
 						window.webkitRequestAnimationFrame ||
@@ -28,6 +29,7 @@ function init(){
 	map.height = heightMap;
 
 	startLoop();
+	spawnOpponent(10);
 
 	addEventListener("keydown", function(event){
 		switch(event.keyCode){
@@ -67,16 +69,14 @@ function update(){
 	draw();
 }
 
-var opponent = {
-	x: 600,
-	y: 400,
-	pW: 100,
-	pH: 100,
-	draw: function(){
-		ctxMap.drawImage(imageOpponent, 0, 0, 150, 150, this.x, this.y, this.pW, this.pH);
-	},
-	clear: function(){
-		ctxMap.clearRect(this.x, this.y, this.x + this.pW, this.y + this.pH);
+function spawnOpponent(count){
+	for (i = 0; i < count; i++){
+		enemies.push({
+			x: Math.floor(Math.random() * widthMap) + widthMap,
+			y: Math.floor(Math.random(0, 0.8) * heightMap),
+			pW: 100,
+			pH: 100,
+		});
 	}
 }
 
@@ -97,14 +97,34 @@ var drawBullet = {
 	}
 }
 
-var kill = 0;
+var drawEnemies = {
+	draw: function(){
+		ctxMap.drawImage(imageOpponent, 0, 0, 100, 100, enemies[i].x, enemies[i].y, 100, 100);
+	} 
+}
+
+var score = 0;
 
 function draw(){
 	ctxMap.clearRect(0, 0, widthMap, heightMap);
 	for (i = 0; i < bullet.length; i++){
 		bullet[i].x += bullet[i].vx;
 		drawBullet.draw();
-		if (((bullet[i].x >= opponent.x) && (bullet[i].x <= (opponent.x + opponent.pW))) && ((bullet[i].y >= opponent.y) && (bullet[i].y <= opponent.y + opponent.pH))) kill = 1;
+		if (bullet[i].x > widthMap) bullet.splice(i, 1);
+	}
+
+	for (i = 0; i < enemies.length; i++){
+		enemies[i].x -= 3;
+		drawEnemies.draw();
+		if (enemies[i].x < 0) {
+			alert(score);
+			score = 0;
+			document.location.reload(true);
+		}
+		if (enemies[i].y > 500) {
+			enemies[i].x = Math.floor(Math.random() * widthMap) + widthMap;
+			enemies[i].y = Math.floor(Math.random(0, 0.8) * heightMap);
+		}
 	}
 
 	timer++;
@@ -113,10 +133,20 @@ function draw(){
 		bullets = 0;
 	}
 
+	for (i = 0; i < bullet.length; i++){
+		for (j = 0; j < enemies.length; j++){
+			if ((bullet[i].x > enemies[j].x) && (bullet[i].x < enemies[j].x + enemies[j].pW) && (bullet[i].y > enemies[j].y) && (bullet[i].y < enemies[j].y + enemies[j].pH)){
+				enemies[j].x = Math.floor(Math.random() * widthMap) + widthMap;
+				enemies[j].y = Math.floor(Math.random(0, 0.7) * heightMap);
+				score++;
+			}
+		}
+	}
+
 	addEventListener("keydown", function(event){
 		switch(event.keyCode){
 			case 32:
-				if (bullets < 10){
+				if (bullets <= 10){
 					bullet.push({
 						x: player.x + player.pW/2,
 						y: player.y + player.pH/2 - 8,
@@ -130,8 +160,4 @@ function draw(){
 	});
 
 	player.draw();
-	opponent.draw();
-	if (kill == 1){
-		opponent.clear();
-	}
 }
